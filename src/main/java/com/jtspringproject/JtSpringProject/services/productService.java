c483deb35990a5eb4f98b5fd571b911ae7f7418b
Developer <developer@example.com>
2026-08-19 03:17:21 +0530

    feat: add pagination and sorting to product and order queries

---
+++ src/main/java/com/jtspringproject/JtSpringProject/services/productService.java
@@ -0,0 +1,44 @@
+package com.jtspringproject.JtSpringProject.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.jtspringproject.JtSpringProject.dao.productDao;
+import com.jtspringproject.JtSpringProject.models.Product;
+
+@Service
+public class productService {
+	private final productDao productDao;
+
+	public productService(productDao productDao) {
+		this.productDao = productDao;
+	}
+
+	public List<Product> getProducts() {
+		return this.productDao.getProducts();
+	}
+
+	public Page<Product> getProductsPaged(Pageable pageable) {
+		return this.productDao.findAll(pageable);
+	}
+
+	public Product addProduct(Product product) {
+		return this.productDao.addProduct(product);
+	}
+
+	public Product getProduct(int id) {
+		return this.productDao.getProduct(id);
+	}
+
+	public Product updateProduct(int id, Product product) {
+		product.setId(id);
+		return this.productDao.updateProduct(product);
+	}
+
+	public boolean deleteProduct(int id) {
+		return this.productDao.deleteProduct(id);
+	}
+}
